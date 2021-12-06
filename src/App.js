c039ee06c38095531a35a9b7f2cb7709e98944ab
boaz tene <boaztene2005@gmail.com>
2021-12-06 14:56:34 +0200

    Added the RadioBox component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CheckBox from './Componenets/CheckBox/check-box';
+import RadioBox from './Componenets/RadioBox/radio-box';
 
 export default class App extends React.Component{
   render() {
     return (
     <div className="app">
       <CheckBox keys={["whoami", "The coolest man on earth", "whoareyou", "who are we"]}>This is the checkbox test.</CheckBox>
+      <RadioBox keys={["whoami", "The coolest man on earth", "whoareyou", "who are we"]}>This is the radiobox test.</RadioBox>
     </div>
       );
   }
---
+++ src/Componenets/RadioBox/radio-box.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./radio-box.css";
+
+/**
+ * This component is the RadioBox, 
+ * he has two props keys [key1, key2, ...],
+ * and the children (the title).
+ */
+export default class RadioBox extends React.Component {
+    static id = 0;
+
+    constructor(props) {
+        super(props);
+
+        this.id = RadioBox.id;
+        RadioBox.id++;
+    }
+
+    /**
+     * Renders the options.
+     * 
+     * @returns A rendered XHTMl object.
+     */
+    renderOptions = () => {
+        return this.props.keys.map(key => {
+            return (
+            <div className="option">
+                <input type="radio" id={key} name={this.id} value={key}/>
+                <label for={key}>{key}</label>
+            </div>
+            );
+        });
+    }
+
+    render() {  
+        return (
+            <div className="radio-box">
+                <p className="title">{this.props.children}</p>
+                {this.renderOptions()}
+            </div>
+        );
+    }
+}
