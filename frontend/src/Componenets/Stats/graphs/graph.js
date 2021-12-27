64f143f974f3e847d95e06a2b17d6f17402aa304
BigShotDEV <elad.perl100@gmail.com>
2021-12-27 13:29:44 +0200

    working on graphs

--- frontend/src/Componenets/Stats/graphs/graph.js
+++ frontend/src/Componenets/Stats/graphs/graph.js
@@ -5,30 +5,45 @@ import { Bar } from 'react-chartjs-2'
 export default class Graph extends React.Component {
     constructor(props) {
         super(props)
-        // this.state = {
-        //     labels: this.props.labels,
-        //     datasets: this.props.data.forEach(element => {
-        //         return {
-        //             label: element.label,
-        //             backgroundColor:'rgba(75,192,192,1)',
-        //             borderColor : 'rgba(0,0,0,1)',
-        //             borderWidth : 2,
-        //             data : element.data
-        //         }
-        //     })
-        // }
         this.state = {
             labels: this.props.labels,
-            datasets: [
-                {
-                    label: this.props.label,
-                    backgroundColor: 'rgba(75,192,192,1)',
-                    borderColor: 'rgba(0,0,0,1)',
-                    borderWidth: 2,
-                    data: this.props.data
-                }
-            ]
+            datasets: []
         }
+        this.state.datasets.push({
+                        label: this.props.label,
+                        backgroundColor: 'rgba(75,192,192,1)',
+                        borderColor: 'rgba(0,0,0,1)',
+                        borderWidth: 2,
+                        data: this.props.data
+                    });
+        this.props.values.forEach((item) => {
+            this.state.datasets.push({
+                label: item.label,
+                backgroundColor:'rgba(75,192,192,1)',
+                borderColor : 'rgba(0,0,0,1)',
+                borderWidth : 2,
+                data : item.data
+            });
+        });
+        // this.state = {
+        //     labels: this.props.labels,
+        //     datasets: [
+        //         {
+        //             label: this.props.label,
+        //             backgroundColor: 'rgba(75,192,192,1)',
+        //             borderColor: 'rgba(0,0,0,1)',
+        //             borderWidth: 2,
+        //             data: this.props.data
+        //         },
+        //         {
+        //             label: "balls",
+        //             backgroundColor: 'rgba(200,40,54,1)',
+        //             borderColor: 'rgba(0,0,0,1)',
+        //             borderWidth: 2,
+        //             data: [10,20,30,40,50]
+        //         }
+        //     ]
+        // }
     }
     render() {
         return (
--- frontend/src/Componenets/Stats/stats.js
+++ frontend/src/Componenets/Stats/stats.js
@@ -8,7 +8,8 @@ export default class Stats extends React.Component {
         return (
             <div className="stats-page">    
                 <Graph labels={['boaz', 'ovadia', 'ziv', 'roni', 'beiman']}
-                       values={[{label:'loser meter', data: [100, 99, 101, 100, 95]}]}
+                       values={[{label: 'loser meter', data: [95, 95, 101, 105, 100]},
+                                {label: 'pro meter', data: [110, 115, 10, 22, 25]}]}
                        label={'loser meter'}
                        data={[100, 99, 101, 100, 95]}/>
                 <Graph/>
