7e8f66aaa80c34fb73864f06a8bde04ce721f48c
BigShotDEV <elad.perl100@gmail.com>
2022-01-02 17:07:05 +0200

    still mapping mongoDB to graphs

--- frontend/src/Componenets/Stats/stats.js
+++ frontend/src/Componenets/Stats/stats.js
@@ -2,13 +2,12 @@ import React from 'react';
 import BarGraph from './graphs/graph';
 import './stats.css'
 
-export default class Stats extends React.Component {    
-    constructor(props){
+export default class Stats extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            labels: [],
-            datas: []
-        }
+            teamsData: []
+        };
 
         let mongoData = [
             {
@@ -18,10 +17,12 @@ export default class Stats extends React.Component {
                 "user_name": "joe",
                 "game_number": 1,
                 "team_number": 3075,
-                "stats":{
-                    "lower": 12,
-                    "upper": 44,
-                    "inner": 8,
+                "stats": {
+                    "basic_score": {
+                        "lower": 12,
+                        "upper": 44,
+                        "inner": 8
+                    },
                     "didTryClimb": true,
                     "didClimb": false,
                     "gotDefended": false,
@@ -35,13 +36,15 @@ export default class Stats extends React.Component {
                 "user_name": "joe",
                 "game_number": 2,
                 "team_number": 3075,
-                "stats":{
-                    "lower": 8,
-                    "upper": 49,
-                    "inner": 13,
+                "stats": {
+                    "basic_score": {
+                        "lower": 8,
+                        "upper": 49,
+                        "inner": 13
+                    },
                     "didTryClimb": true,
                     "didClimb": true,
-                    "gotDefended": false,
+                    "gotDefended": true,
                     "defended": false
                 }
             },
@@ -49,40 +52,61 @@ export default class Stats extends React.Component {
                 "_id": {
                     "$oid": "61cecd20784c687627c3a10d"
                 },
-                "user_name": "steve",
+                "user_name": "joe",
                 "game_number": 1,
-                "team_number": 3075,
-                "stats":{
-                    "lower": 15,
-                    "upper": 44,
-                    "inner": 8,
+                "team_number": 3076,
+                "stats": {
+                    "basic_score": {
+                        "lower": 45,
+                        "upper": 14,
+                        "inner": 2
+                    },
                     "didTryClimb": true,
-                    "didClimb": false,
-                    "wasDefended": false,
-                    "defended": false
+                    "didClimb": true,
+                    "gotDefended": false,
+                    "defended": true
                 }
             }
-        ]
+        ];
 
-        let teamsData = {
-        }
+        let rawTeamsData = {
+        };
 
-        mongoData.forEach((item, index) => {
-            if (teamsData[item.team_number] === undefined){
-                
-            } 
+        mongoData.forEach(item => {
+            if (rawTeamsData[item.team_number] === undefined) {
+                rawTeamsData[item.team_number] = {};
+            }
+            rawTeamsData[item.team_number][item.game_number] = item.stats;
         });
+
+        for (const [team, games] of Object.entries(rawTeamsData)) {
+            let labels = [];
+            Object.keys(games).forEach(key => {
+                let label = 'game number #' + key;
+                if (games[key].gotDefended) {
+                    label += " (got defended)";
+                } else if (games[key].defended) {
+                    label += " (defended)";
+                }
+                labels.push(label);
+            })
+            
+            let graphValues = [];
+
+
+            this.state.teamsData.push(
+                <div className="stats-page">
+                    <h1>{team}</h1>
+                    <BarGraph labels={labels}
+                        values={[{ label: 'total', data: [71, 80, 72], stack: "total" },
+                        { label: 'bottom', data: [20, 20, 20], stack: "holes" },
+                        { label: 'top', data: [20, 28, 18], stack: "holes" },
+                        { label: 'inner', data: [30, 3, 25], stack: "holes" }]} />
+                </div>
+            )
+        }
     }
     render() {
-        return (
-            <div className="stats-page">    
-                <BarGraph labels={['game 1', 'game 2', 'game 3']}
-                       values={[{label: 'total', data: [71, 80, 72], stack: "total"},
-                                {label: 'bottom', data: [20, 20, 20], stack: "holes"},
-                                {label: 'top', data: [20, 28, 18], stack: "holes"},
-                                {label: 'inner', data: [30, 3, 25], stack: "holes"}]}/>
-                <p>Climbed: {this.state.test}</p>
-            </div>
-        )
+        return this.state.teamsData;
     }
 }
