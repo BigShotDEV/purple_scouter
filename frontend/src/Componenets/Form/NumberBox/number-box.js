705a34960e28ea9350797588164558baedfacbaa
boaz tene <boaztene2005@gmail.com>
2022-02-13 01:26:46 +0200

    Added a restrication on the user input, now the user can only enter numbers in the number-box

--- frontend/src/Componenets/Form/NumberBox/number-box.js
+++ frontend/src/Componenets/Form/NumberBox/number-box.js
@@ -7,12 +7,27 @@ import "./number-box.css";
  * and it gets a onChange callback. 
  */
 export default class NumberBox extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.input_ref = React.createRef();
+    }
+
+    componentDidMount() {
+        // making sure the user inputs only numric values.
+        this.input_ref.current.addEventListener("keypress", function (evt) {
+            if (evt.which < 48 || evt.which > 57) { // only allows numbers
+                evt.preventDefault();
+            }
+        });
+    }
+
     render() {
         return (
             <div className="number-box">
                 <h1>{this.props.children}</h1>
 
-                <input type="number" onChange={(event) => {this.props.onChange(event, this.props.id)}} defaultValue={this.props.default ? this.props.default : ""}></input>
+                <input type="number" ref={this.input_ref} onChange={(event) => {this.props.onChange(event, this.props.id)}} defaultValue={this.props.default ? this.props.default : ""}></input>
             </div>
         );  
     }
